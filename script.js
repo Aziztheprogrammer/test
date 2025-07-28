1cf21f4d905e4d5bb5ec20121ca87bfbc2a322bd
Aziz Berhouma <aziz.theprogrammer@gmail.com>
2025-07-28 14:37:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nextButton = document.querySelector("button.next")
 const prevButton = document.querySelector("button.prev")
 const testimonialsContainer = document.querySelector(".testimonials-carousel")
 
-nextButton.addEventListener("click", (traget) => {
+nextButton.addEventListener("click", (target) => {
     testimonialsContainer.scrollBy({
         left: 100,
         top: 0,
@@ -10,10 +10,31 @@ nextButton.addEventListener("click", (traget) => {
     })
 })
 
-prevButton.addEventListener("click", (traget) => {
+prevButton.addEventListener("click", (target) => {
     testimonialsContainer.scrollBy({
         left: -100,
         top: 0,
         behavior: "smooth"
     })
 })
+
+let x = 2
+
+autoScrollTest = setInterval(() => {
+    testimonialsContainer.scrollBy({
+        left: x,
+        top: 0,
+        behavior: "smooth"
+    })
+
+    testimonialsContainer.onscrollend = (event) => {
+        // console.log("helooooo")
+        // clearInterval(autoScrollTest)
+        x = -2
+    }
+
+    if (testimonialsContainer.scrollLeft == 0) {
+        x = 2
+    }
+
+}, 5)
